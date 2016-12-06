03e111afed79d05c17963b02589c0c5c24cf66c5
Felix Törnqvist <felix.trnqvist@gmail.com>
2016-12-06 13:17:32 +0100

    Sprite is now TexturedSprite

--- src/Sprite.cpp
+++ src/Sprite.cpp
@@ -1,19 +1,38 @@
-#include <iostream>
 #include "Sprite.h"
+#include "System.h"
 
+#ifdef __APPLE__
+#include <SDL2/SDL.h>
+#include <SDL2_image/SDL_image.h>
+
+#else
+#include <SDL.h>
+#include <SDL_image.h>
+#endif
+
+
+#include <string>
+#include <stdexcept>
 
 namespace mcDirr {
-	Sprite::Sprite(std::string message):msg(message) {
-		//ctor
+	Sprite::Sprite(const std::string filePath){
+		SDL_Surface* surface = IMG_Load(filePath.c_str());
+		if(surface == nullptr)
+			throw std::runtime_error(std::string("could not load texture:") + SDL_GetError());
+		texture = SDL_CreateTextureFromSurface(sys.getRen(), surface);
+		SDL_FreeSurface(surface);
 	}
 
-	void Sprite::tick(int timeDiff) {
+	void const Sprite::draw(){
+		SDL_Rect r = {10, 10, 400, 400};
+		SDL_RenderCopy(sys.getRen(), texture, NULL, &r);
 	}
 
-	void const Sprite::draw() {
+	void Sprite::tick(int timediff){
+
 	}
 
 	Sprite::~Sprite() {
-		//dtor
+		SDL_DestroyTexture(texture);
 	}
 }
