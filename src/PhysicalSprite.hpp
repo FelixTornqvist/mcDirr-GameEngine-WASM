26749b30c32b30369f8ca72e17c0a460b5988cb9
Annika Svedin <annika.svedin@gmail.com>
2016-12-07 10:59:23 +0100

    PhysicalSprite. hpp, cpp

---
+++ src/PhysicalSprite.hpp
@@ -0,0 +1,14 @@
+//
+//  PhysicalSprite.hpp
+//  mcDirr
+//
+//  Created by Per Andersen on 06/12/16.
+//  Copyright © 2016 dsv. All rights reserved.
+//
+
+#ifndef PhysicalSprite_hpp
+#define PhysicalSprite_hpp
+
+#include <stdio.h>
+
+#endif /* PhysicalSprite_hpp */
