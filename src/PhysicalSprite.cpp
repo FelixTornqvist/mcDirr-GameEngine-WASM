e9b3603270b9bb1ec0a71c3cbe42ef151a1a23d5
Annika Svedin <annika.svedin@gmail.com>
2016-12-07 11:00:29 +0100

    some code in PhysicalSprite, not finished

--- src/PhysicalSprite.cpp
+++ src/PhysicalSprite.cpp
@@ -1,9 +1,31 @@
-//
-//  PhysicalSprite.cpp
-//  mcDirr
-//
-//  Created by Per Andersen on 06/12/16.
-//  Copyright © 2016 dsv. All rights reserved.
-//
+#ifdef __APPLE__
+#include <SDL2/SDL.h>
+
+#else
+#include <SDL.h>
+#endif
 
 #include "PhysicalSprite.hpp"
+#include "System.hpp"
+
+
+
+using namespace mcDirr;
+
+PhysicalSprite::PhysicalSprite(SDL_Texture* t, int x, int y): Sprite(t, x, y)
+{ currentTime = 0;}
+
+        
+void PhysicalSprite::draw() const override {
+    SDL_RenderCopyEx(sys.getRen(), texture, NULL, &dest, currentTime, NULL, SDL_FLIP_NONE);
+}
+
+void PhysicalSprite::tick(int time) override {
+    currentTime += time;
+    dest.x ++;
+    dest.y ++;
+}
+
+//Sprite::~Sprite() {
+//    SDL_DestroyTexture(texture);
+//}
