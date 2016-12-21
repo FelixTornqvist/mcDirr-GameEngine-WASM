12e57e779bee74d313b227942b6bdeea2d949a1c
Felix Törnqvist <felix.trnqvist@gmail.com>
2016-12-21 12:25:17 +0100

    WIP

--- src/Main.cpp
+++ src/Main.cpp
@@ -63,14 +63,14 @@ int main(int argc, char** argv) {
 	//Sprite* s1 = new AnimatedSprite(texture, 10, 10, 4, 1000);
 	//Sprite* s2 = new CustSprite(texture, 300, 250, 0, -1);
 
-	PhysicalSprite* s15 = PhysicalSprite::getInstance(surf, 600, 200, 0, true);
-	PhysicalSprite* s16 = PhysicalSprite::getInstance(surf, 800, 200, 0.5, true);
+	PhysicalSprite* s15 = PhysicalSprite::getInstance(surf, 600, 200, 0, false);
+	PhysicalSprite* s16 = PhysicalSprite::getInstance(surf, 800, 200, 0.5, false);
 
-	PhysicalSprite* s17 = PhysicalSprite::getInstance(surf, 400, 600, 0, false);
-	PhysicalSprite* s18 = PhysicalSprite::getInstance(surf, 500, 600, 0, false);
-	PhysicalSprite* s19 = PhysicalSprite::getInstance(surf, 600, 600, 0, false);
-	PhysicalSprite* s20 = PhysicalSprite::getInstance(surf, 700, 600, 0, false);
-	PhysicalSprite* s21 = PhysicalSprite::getInstance(surf, 800, 600, 0, false);
+	PhysicalSprite* s17 = PhysicalSprite::getInstance(surf, 400, 600, 0, true);
+	PhysicalSprite* s18 = PhysicalSprite::getInstance(surf, 500, 600, 0, true);
+	PhysicalSprite* s19 = PhysicalSprite::getInstance(surf, 600, 600, 0, true);
+	PhysicalSprite* s20 = PhysicalSprite::getInstance(surf, 700, 600, 0, true);
+	PhysicalSprite* s21 = PhysicalSprite::getInstance(surf, 800, 600, 0, true);
 
 	Level* lvl2 = Level::getInstance(nullptr);
 
@@ -83,21 +83,21 @@ int main(int argc, char** argv) {
 	lvl2->add(s21);
 
 
-	PhysicalSprite* s1 = PhysicalSprite::getInstance(surf, 200, 200, 0, false); // last int is for speed. 0 cant be moved.
-	PhysicalSprite* s2 = PhysicalSprite::getInstance(surf, 400, 200, 0, true);
-	PhysicalSprite* s3 = PhysicalSprite::getInstance(surf, 600, 200, 0, true);
-	PhysicalSprite* s4 = PhysicalSprite::getInstance(surf, 800, 200, 0.5, true); // can be moved. Remove last int later. only to test collision.
+	PhysicalSprite* s1 = PhysicalSprite::getInstance(surf, 200, 200, 0, true); // last int is for speed. 0 cant be moved.
+	PhysicalSprite* s2 = PhysicalSprite::getInstance(surf, 400, 200, 0, false);
+	PhysicalSprite* s3 = PhysicalSprite::getInstance(surf, 600, 200, 0, false);
+	PhysicalSprite* s4 = PhysicalSprite::getInstance(surf, 800, 200, 0.9, false); // can be moved. Remove last int later. only to test collision.
 
-	PhysicalSprite* s5 = PhysicalSprite::getInstance(surf, 200, 600, 0, false);
-	PhysicalSprite* s6 = PhysicalSprite::getInstance(surf, 300, 600, 0, false);
-	PhysicalSprite* s7 = PhysicalSprite::getInstance(surf, 400, 600, 0, false);
-	PhysicalSprite* s8 = PhysicalSprite::getInstance(surf, 500, 600, 0, false);
-	PhysicalSprite* s9 = PhysicalSprite::getInstance(surf, 600, 600, 0, false);
-	PhysicalSprite* s10 = PhysicalSprite::getInstance(surf, 700, 600, 0, false);
-	PhysicalSprite* s11 = PhysicalSprite::getInstance(surf, 800, 600, 0, false);
-	PhysicalSprite* s12 = PhysicalSprite::getInstance(surf, 900, 600, 0, false);
-	PhysicalSprite* s13 = PhysicalSprite::getInstance(surf, 100, 600, 0, false);
-	PhysicalSprite* s14 = PhysicalSprite::getInstance(surf, 0, 600, 0, false);
+	PhysicalSprite* s5 = PhysicalSprite::getInstance(surf, 200, 600, 0, true);
+	PhysicalSprite* s6 = PhysicalSprite::getInstance(surf, 300, 600, 0, true);
+	PhysicalSprite* s7 = PhysicalSprite::getInstance(surf, 400, 600, 0, true);
+	PhysicalSprite* s8 = PhysicalSprite::getInstance(surf, 500, 600, 0, true);
+	PhysicalSprite* s9 = PhysicalSprite::getInstance(surf, 600, 600, 0, true);
+	PhysicalSprite* s10 = PhysicalSprite::getInstance(surf, 700, 600, 0, true);
+	PhysicalSprite* s11 = PhysicalSprite::getInstance(surf, 800, 600, 0, true);
+	PhysicalSprite* s12 = PhysicalSprite::getInstance(surf, 900, 600, 0, true);
+	PhysicalSprite* s13 = PhysicalSprite::getInstance(surf, 100, 600, 0, true);
+	PhysicalSprite* s14 = PhysicalSprite::getInstance(surf, 0, 600, 0, true);
 
 	Level* lvl1 = Level::getInstance(lvl2);
 
