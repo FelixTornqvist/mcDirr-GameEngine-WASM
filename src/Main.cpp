725d91317940cfd9620e869e9ab192362918f47c
Felix Törnqvist <felix.trnqvist@gmail.com>
2017-02-07 12:27:35 +0100

    level 2 changes

--- src/Main.cpp
+++ src/Main.cpp
@@ -29,18 +29,16 @@ void freeFunk(){
 int main(int argc, char** argv) {
 	//test code
 	GameEngine ge = GameEngine("mcDirr - The gaem", W_WIDTH, W_HEIGHT, FPS);
-	SDL_Surface* cat = loader.loadSurface("media/tinyCat.png");
-
 	Mix_Chunk* BGmusic = loader.loadWAV("media/bgMusic.wav");
 	Mix_PlayChannel(-1, BGmusic, -1);
 
+	SDL_Surface* grass = loader.loadSurface("media/grass.png");
+	SDL_Surface* cat = loader.loadSurface("media/tinyCat.png");
+
 	MobileSprite* s15 = MobileSprite::getInstance(cat, 600, 200);
 	MobileSprite* s16 = MobileSprite::getInstance(cat, 800, 200);
-	ImmobileSprite* s17 = ImmobileSprite::getInstance(cat, 400, 600, 0.5);
-	ImmobileSprite* s18 = ImmobileSprite::getInstance(cat, 500, 600, 0.5);
-	ImmobileSprite* s19 = ImmobileSprite::getInstance(cat, 600, 600, 0.5);
-	ImmobileSprite* s20 = ImmobileSprite::getInstance(cat, 700, 600, 0.5);
-	ImmobileSprite* s21 = ImmobileSprite::getInstance(cat, 800, 600, 0.5);
+	ImmobileSprite* s17 = ImmobileSprite::getInstance(grass, 400, 600, 0.2);
+	ImmobileSprite* s18 = ImmobileSprite::getInstance(grass, -500, 400, 0.2);
 
 	Level* lvl2 = Level::getInstance(nullptr);
 
@@ -48,14 +46,10 @@ int main(int argc, char** argv) {
 	lvl2->add(s16);
 	lvl2->add(s17);
 	lvl2->add(s18);
-	lvl2->add(s19);
-	lvl2->add(s20);
-	lvl2->add(s21);
 
 
 	SDL_Surface* lvl1BGSurf = loader.loadSurface("media/level1BG.png");
 	SDL_Surface* santasheet = loader.loadSurface("media/santa-sheet.png");
-	SDL_Surface* grass = loader.loadSurface("media/grass.png");
 	SDL_Surface* slime = loader.loadSurface("media/slime.png");
 
 	VisualSprite* lvl1BG = VisualSprite::getInstance(lvl1BGSurf, 0, 0);
