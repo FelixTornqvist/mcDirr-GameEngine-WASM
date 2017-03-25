e30537ae756c641a3cd86fd393923dab117bab87
Pontus <Pontussandliden@hotmail.com>
2017-03-25 19:18:57 +0100

    fixed problem

--- src/Main.cpp
+++ src/Main.cpp
@@ -34,7 +34,7 @@ using namespace mcDirr;
 
 
 GameEngine ge = GameEngine("mcDirr - The gaem", W_WIDTH, W_HEIGHT, FPS);
-Level *lvl1, *lvl2, *lvl3;
+Level *lvl1, *lvl2, *lvl3, *lvl4;
 GUIScreen* start;
 std::shared_ptr<GUIText> text;
 std::shared_ptr<GUITextField> nameInput, nameInput2;
@@ -76,50 +76,60 @@ int main(int argc, char** argv) {
 	SDL_Surface* evilCat = loader.loadSurface("media/extraevilCat.png");
 	SDL_Texture* hearts = loader.loadTexture("media/heart.png");
 
+	SDL_Surface* lvl1BGSurf = loader.loadSurface("media/level1BG.png");
+	SDL_Surface* lvl2BGSurf = loader.loadSurface("media/level1BG.png");
+	SDL_Surface* lvl3BGSurf = loader.loadSurface("media/level1BG.png");
+	SDL_Surface* lvl4BGSurf = loader.loadSurface("media/level1BG.png");
+
+	SDL_Surface* santasheet = loader.loadSurface("media/santa-sheet.png");
+	SDL_Surface* fireballSheet = loader.loadSurface("media/fireball.png");
+	SDL_Surface* slime = loader.loadSurface("media/slime.png");
+
+	santa = SantaHero::getInstance(santasheet, fireballSheet, 50, 50, 11, 10, hearts, &ge);
+	santa->setHealth(4);
+	santa->showHealth(true);
+
 	std::shared_ptr<MobileSprite> s15 = EvilCat::getInstance(cat, 600, 200, hearts);
 	std::shared_ptr<MobileSprite> s16 = EvilCat::getInstance(cat, 800, 200, hearts);
 
 	std::shared_ptr<ImmobileSprite> s17 = Obstacle::getInstance(grass, 400, 600, 0.2f);
 	std::shared_ptr<ImmobileSprite> s18 = Obstacle::getInstance(grass, -500, 400, 0.2f);
 
-	std::shared_ptr<ImmobileSprite> l3o1 = Obstacle::getInstance(grass, 400, 600, 0.2f);
-	std::shared_ptr<ImmobileSprite> l3o2 = Obstacle::getInstance(grass, 500, 600, 0.2f);
-	std::shared_ptr<ImmobileSprite> l3o3 = Obstacle::getInstance(grass, 1, 600, 0.2f);
+	std::shared_ptr<ImmobileSprite> l3o1 = Obstacle::getInstance(grass, 5, 700, 0.2f);
+
+	std::shared_ptr<ImmobileSprite> l4o1 = Obstacle::getInstance(grass, 30, 600, 0.2f);
 
 	lvl1 = Level::getInstance(10, 400, 700, 400);
-	lvl2 = Level::getInstance(10,190,800,500);
-	lvl3 = Level::getInstance(200,100,800,500);
+	lvl2 = Level::getInstance(1,190,800,500);
+	lvl3 = Level::getInstance(10,l3o1->getY() - santa->getHeight() - 1 ,850, l3o1->getY() - santa->getHeight() - 1);
+	lvl4 = Level::getInstance(30, l4o1->getY() - santa->getHeight() - 1, 10, 10);
 
-	lvl3->add(l3o3);
-	lvl3->add(l3o3);
-	lvl3->add(l3o3);
+	lvl3->add(l3o1);
+
+	lvl4->add(l4o1);
+
 	lvl2->add(s15);
 	lvl2->add(s16);
 	lvl2->add(s17);
 	lvl2->add(s18);
 
-	SDL_Surface* lvl1BGSurf = loader.loadSurface("media/level1BG.png");
-	SDL_Surface* lvl2BGSurf = loader.loadSurface("media/level1BG.png");
-	SDL_Surface* lvl3BGSurf = loader.loadSurface("media/level1BG.png");
-
-	SDL_Surface* santasheet = loader.loadSurface("media/santa-sheet.png");
-	SDL_Surface* fireballSheet = loader.loadSurface("media/fireball.png");
-	SDL_Surface* slime = loader.loadSurface("media/slime.png");
-
 	std::shared_ptr<Sprite> lvl1BG = Background::getInstance(lvl1BGSurf, 0, 0);
 	std::shared_ptr<Sprite> lvl2BG = Background::getInstance(lvl2BGSurf, 0, 0);
 	std::shared_ptr<Sprite> lvl3BG = Background::getInstance(lvl3BGSurf, 0, 0);
+	std::shared_ptr<Sprite> lvl4BG = Background::getInstance(lvl4BGSurf, 0, 0);
 
+	lvl1->add(lvl1BG);
 	lvl2->add(lvl2BG);
 	lvl3->add(lvl3BG);
+	lvl4->add(lvl4BG);
 
 	std::shared_ptr<MobileSprite> s2 = EvilCat::getInstance(cat, 200, 100, hearts);
 	std::shared_ptr<MobileSprite> s3 = EvilCat::getInstance(cat, 600, 200, hearts);
-	std::shared_ptr<MobileSprite> l3ec1 = EvilCat::getInstance(evilCat, 600, 500, hearts);
+	std::shared_ptr<MobileSprite> l3ec1 = EvilCat::getInstance(evilCat, 600, 300, hearts);
 
 	l3ec1->setHealth(20);
 	l3ec1->showHealth(true);
-	lvl3->add(l3ec1);
+	//lvl3->add(l3ec1);
 
 	s2->setHealth(40);
 	s3->setHealth(40);
@@ -129,11 +139,6 @@ int main(int argc, char** argv) {
 	std::shared_ptr<ImmobileSprite> s5 = Obstacle::getInstance(grass, 10, 600, 0.2f);
 	std::shared_ptr<ImmobileSprite> s8 = Obstacle::getInstance(slime, 500, 510, 1.1f);
 
-	lvl1 = Level::getInstance(10,400,700,400);
-
-	santa = SantaHero::getInstance(santasheet, fireballSheet, 50, 50, 11, 10, hearts, &ge);
-	santa->setHealth(4);
-	santa->showHealth(true);
 	//SantaHero* santa2 = SantaHero::getInstance(santasheet, fireballSheet, 300, 300, 11, 10, hearts, &ge);
 
 	text = GUIText::getInstance(ubuntuB, {0,0,0}, "McDirr - the Gaem!");
@@ -148,7 +153,6 @@ int main(int argc, char** argv) {
 
 	lvl1->add(s5);
 	lvl1->add(s8);
-	lvl1->add(lvl1BG);
 	lvl1->add(text);
 
 
@@ -182,6 +186,7 @@ int main(int argc, char** argv) {
 	ge.addScreen(lvl1);
 	ge.addScreen(lvl2);
 	ge.addScreen(lvl3);
+	ge.addScreen(lvl4);
 
 
 	sys.addKeyFunction<GameEngine>(SDLK_l, &ge, &GameEngine::nextScreen);
